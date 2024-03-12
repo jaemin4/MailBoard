b6e2cf61fd54d127146c39aaadc0070b13d686de
eheh4 <eheh4@192.168.1.115>
2024-03-12 10:38:41 +0900

    게시판 조회 기능  commit - 재민

--- SpringMVC01/src/main/java/kr/smhrd/controller/BoardController.java
+++ SpringMVC01/src/main/java/kr/smhrd/controller/BoardController.java
@@ -3,15 +3,20 @@ package kr.smhrd.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.smhrd.entity.Board;
+import kr.smhrd.mapper.BoardMapper;
 
 @Controller
 public class BoardController {
 
+
+	@Autowired
+	private BoardMapper mapper;
 	
 	@GetMapping("/home.do")
 	public String GoHome() {
@@ -21,17 +26,8 @@ public class BoardController {
 	
 	@GetMapping("/boardList.do")
 	public String boardList(Model model) {
-		Board vo = new Board();
-		vo.setIdx(1);
-		vo.setTitle("게시판 실습");
-		vo.setContent("게시판 실습");
-		vo.setWriter("오재민");
-		vo.setIndate("23");
-		
-		
-		List<Board> list = new ArrayList<Board>();
-		list.add(vo);
-		
+
+		List<Board> list = mapper.getLists();
 		model.addAttribute("list",list);
 		
 		return "board/boardList";
